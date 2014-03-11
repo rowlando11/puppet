ab44f6e7800ddfa77ce7ff8b26e88873241fc242
rowlando <rowland.ono@gmail.com>
2014-03-11 13:23:54 +0000

    added nagios module

---
+++ nagios/manifests/monitor.pp
@@ -0,0 +1,24 @@
+# Manage the Nagios monitoring service
+class nagios::monitor {
+  # Manage the packages
+  package { [ "nagios", "nagios-plugins" ]: ensure => installed }
+  # Manage the Nagios monitoring service
+  service { "nagios":
+    ensure    => running,
+    hasstatus => true,
+    enable    => true,
+    subscribe => [ Package["nagios"], Package["nagios-plugins"] ],
+}
+
+	
+	file { '/etc/nagios/nagios.cfg':
+      ensure => present,
+      mode   => 644,
+	  audit => all,
+      source => 'puppet:///modules/nagios/nagios.cfg',
+	  notify => Service["nagios"],
+    }
+  # collect resources and populate /etc/nagios/nagios_*.cfg
+  Nagios_host    <<||>> { notify => Service["nagios"] }
+  Nagios_service <<||>> { notify => Service["nagios"] }
+}
---
+++ nagios/manifests/target.pp
@@ -0,0 +1,27 @@
+class nagios::target {
+
+  @@nagios_host { "$fqdn":
+    ensure  => present,
+    alias   => $hostname,
+    address => $ipaddress,
+    use     => "linux-server",
+	}
+
+  @@nagios_service { "check_ping_${hostname}":
+	check_command => "check_ping!100.0,20%!500.0,60%", 
+	use		=>   "generic-service",
+	host_name => "$fqdn",
+	notification_period => "24x7",
+	service_description => "${hostname}_check_ping"
+	} 
+
+
+  @@nagios_service { "check_load_${hostname}":
+    check_command => "check_nrpe!check_load", 
+    use     =>   "generic-service",
+    host_name => "$fqdn",
+    notification_period => "24x7",
+    service_description => "${hostname}_check_load"
+    }   
+
+}	 
