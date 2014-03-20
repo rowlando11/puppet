71403c53f2882fad52b09c55a738ed0aa818e7c3
rowlando <rowland.ono@gmail.com>
2014-03-20 12:19:57 +0000

    amended target

--- nagios/manifests/target.pp
+++ nagios/manifests/target.pp
@@ -9,7 +9,7 @@ class nagios::target {
   }
 
   @@nagios_service { "check_ping_${hostname}":
-    check_command       => "check_ping!100.0,20%!500.0,60%",
+    check_command       => 'check_ping!100.0,20%!500.0,60%',
     use                 => "generic-service",
     host_name           => "$fqdn",
     notification_period => "24x7",
@@ -18,7 +18,7 @@ class nagios::target {
 
 
   @@nagios_service { "check_load_${hostname}":
-    check_command       => "check_nrpe!check_load",
+    check_command       => 'check_nrpe!check_load',
     use                 =>   "generic-service",
     host_name           => "$fqdn",
     notification_period => "24x7",
