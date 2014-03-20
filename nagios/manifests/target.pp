35a7b25c76589b1ba0dcb108161d21055963cece
rowlando <rowland.ono@gmail.com>
2014-03-20 11:53:19 +0000

    updated nagios target file

--- nagios/manifests/target.pp
+++ nagios/manifests/target.pp
@@ -1,3 +1,4 @@
+# nagios host and service config file
 class nagios::target {
 
   @@nagios_host { "$fqdn":
@@ -5,23 +6,40 @@ class nagios::target {
     alias   => $hostname,
     address => $ipaddress,
     use     => "linux-server",
-	}
+  }
 
   @@nagios_service { "check_ping_${hostname}":
-	check_command => "check_ping!100.0,20%!500.0,60%", 
-	use		=>   "generic-service",
-	host_name => "$fqdn",
-	notification_period => "24x7",
-	service_description => "${hostname}_check_ping"
-	} 
+    check_command       => "check_ping!100.0,20%!500.0,60%",
+    use                 => "generic-service",
+    host_name           => "$fqdn",
+    notification_period => "24x7",
+    service_description => "${hostname}_check_ping"
+  }
 
 
   @@nagios_service { "check_load_${hostname}":
-    check_command => "check_nrpe!check_load", 
-    use     =>   "generic-service",
-    host_name => "$fqdn",
+    check_command       => "check_nrpe!check_load",
+    use                 =>   "generic-service",
+    host_name           => "$fqdn",
     notification_period => "24x7",
     service_description => "${hostname}_check_load"
-    }   
+  } 
 
-}	 
+  @@nagios_service { "check_nrpe_${hostname}":
+    check_command       => "check_tcp!5666",
+    use                 => "generic-service",
+    host_name           => "$fqdn",
+    notification_period => "24x7",
+    service_description => "${hostname}_check_nrpe",
+  }
+
+  @@nagios_service { "check_http_${hostname}":
+    check_command       => "check_tcp!80",
+    use                 => "generic-service",
+    host_name           => "$fqdn",
+    notification_period => "24x7",
+    service_description => "${hostname}_check_http",
+  }   
+
+}
+
