b8d3d0cb44a8ba880c170d726e74efe4855a8142
rowlando <rowland.ono@gmail.com>
2014-03-20 13:18:39 +0000

    Updated fail2ban install file

--- nagios/manifests/target.pp
+++ nagios/manifests/target.pp
@@ -8,20 +8,36 @@ class nagios::target {
 	}
 
   @@nagios_service { "check_ping_${hostname}":
-	check_command => "check_ping!100.0,20%!500.0,60%", 
-	use		=>   "generic-service",
-	host_name => "$fqdn",
+	check_command       => "check_ping!100.0,20%!500.0,60%",
+	use		            => "generic-service",
+	host_name           => "$fqdn",
 	notification_period => "24x7",
 	service_description => "${hostname}_check_ping"
-	} 
+	}
 
 
   @@nagios_service { "check_load_${hostname}":
-    check_command => "check_nrpe!check_load", 
-    use     =>   "generic-service",
-    host_name => "$fqdn",
+    check_command       => "check_nrpe!check_load",
+    use                 =>   "generic-service",
+    host_name           => "$fqdn",
     notification_period => "24x7",
     service_description => "${hostname}_check_load"
+    } 
+
+  @@nagios_service { "check_nrpe_${hostname}":
+    check_command      => "check_tcp!5666",
+	use                => "generic-service",
+	host_name          => "$fqdn",
+	notification_period => "24x7",
+	service_description => "${hostname}_check_nrpe",
+    }
+
+  @@nagios_service { "check_http_${hostname}":
+    check_command      => "check_tcp!80",
+    use                => "generic-service",
+    host_name          => "$fqdn",
+    notification_period => "24x7",
+    service_description => "${hostname}_check_http",
     }   
 
-}	 
+}
