13a887402183725f032e96ca8029ce8a8aab9b86
rowlando <rowland.ono@gmail.com>
2014-03-23 20:01:14 +0000

    changes to epel.pp

--- yumrepo/manifests/epel.pp
+++ yumrepo/manifests/epel.pp
@@ -1,6 +1,24 @@
-# yum epel repo install
+# yum epel repo install for enterprise linux 6
 class yumrepo::epel {
-  package { 'epel-release':
-    ensure => present,
+
+  if ($::operatingsystem == 'CentOS') and ($::operatingsystemrelease =~ /^6/) {
+    yumrepo { 'epel-release':
+      name           => 'Extra Packages for Enterprise Linux 6',
+      mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
+      enabled        => 'Yes',
+      failovermethod => 'priority',
+      gpgkey         => '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+      gpgcheck       => 'Yes',
+      require        => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6'],
+    }
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6':
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => 'puppet:///modules/yumrepo/rpm-gpg/RPM-GPG-KEY-EPEL',
+    }
   }
+    else {
+    notify { 'We are installing this repo on CentOS versions 6 only!!':}
+    }
 }
