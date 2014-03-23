da246ed472f7de5f1bf3cd7e3ec6a36a3e3165ef
rowlando <rowland.ono@gmail.com>
2014-03-23 13:24:35 +0000

    changes to epel manifest

--- yumrepo/manifests/epel-release.pp
+++ yumrepo/manifests/epel-release.pp
@@ -1,6 +1,6 @@
 # yum epel repo install
 class yumrepo::epel-release {
-  package {'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+  package {'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm':
   ensure => present,
   }
 }
