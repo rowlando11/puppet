e927760e0d1a44a16a68dd649fc9940f248dc629
rowlando <rowland.ono@gmail.com>
2014-03-23 13:12:04 +0000

    changes to epel repo install

--- yumrepo/manifests/epel-release.pp
+++ yumrepo/manifests/epel-release.pp
@@ -1,5 +1,6 @@
+# yum epel repo install
 class yumrepo::epel-release {
-	package { epel-release: 
-		ensure => present 
-	}
+  package {'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+  ensure => present,
+  }
 }
