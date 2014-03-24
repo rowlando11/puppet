5faf0ddc07cffd5a0195e15c2d5f31fd7e816ed9
rowlando <rowland.ono@gmail.com>
2014-03-24 13:56:52 +0000

    more lint compatible changes

--- mysql/manifests/config.pp
+++ mysql/manifests/config.pp
@@ -1,14 +1,11 @@
+# mysql server config
 class mysql::config {
-	file { "/etc/my.cnf":
-		ensure => present,
-		source => "puppet:///modules/mysql/my.cnf",
-		owner => "mysql",
-		group => "mysql",
-		require => Class["mysql::install"],
-		notify => Class["mysql::service"],
-	}
-
-#	file { '/etc/hosts':
-#		audit => all,
-#	}
+  file { '/etc/my.cnf':
+    ensure  => present,
+    source  => 'puppet:///modules/mysql/my.cnf',
+    owner   => 'mysql',
+    group   => 'mysql',
+    require => Class['mysql::install'],
+    notify  => Class['mysql::service'],
+  }
 }
